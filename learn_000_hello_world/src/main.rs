09329eac25db3992e9be109c3892e25fcddbe1ac
ericmegalovania <1486853661@qq.com>
2026-03-03 20:40:35 +0800

    test iter

--- learn_000_hello_world/src/main.rs
+++ learn_000_hello_world/src/main.rs
@@ -3,7 +3,9 @@ fn greet_world() {
     let chinese = "世界，你好";
     let english = "World, hello";
     let regions = [southern_germany, chinese, english];
-    for region in regions.iter() {
+    for region in regions
+    /*.iter()*/
+    {
         println!("{}", &region);
     }
 }
